6441c0fe18336e319f4c72d60822ed0eba262608
Brittany Javalera <brittjavalera@gmail.com>
2020-01-10 16:29:38 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskList = document.getElementById('tasks')
+  const taskForm = document.getElementById('create-task-form')
+    taskForm.addEventListener('submit', function(event) {
+      event.preventDefault() //stop form from posting
+      const userInputField = event.target.querySelector('#new-task-description')
+      const userInputString = userInputField.value
+      const taskListItem = document.createElement('li')
+      taskListItem.textContent = userInputString
+      taskList.appendChild(taskListItem)
+
+    });
 });
